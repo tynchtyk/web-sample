87583357e216c537510f4ba1b734f3494c91ad5d
Tynchtykbek Kaldybaev <tynchtykkaldybaev@gmail.com>
2019-08-22 15:17:06 +0900

    https

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const HTTPS_PORT = 8443;
 
 const fs = require('fs');
-const https = require('http');
+const https = require('https');
 const WebSocket = require('ws');
 const WebSocketServer = WebSocket.Server;
 
@@ -51,7 +51,7 @@ wss.broadcast = function(data) {
   });
 };
 
-console.log('Server running. Visit http://localhost:' + HTTPS_PORT + ' in Firefox/Chrome.\n\n\
+console.log('Server running. Visit https://localhost:' + HTTPS_PORT + ' in Firefox/Chrome.\n\n\
 Some important notes:\n\
   * Note the HTTPS; there is no HTTP -> HTTPS redirect.\n\
   * You\'ll also need to accept the invalid TLS certificate.\n\
